51c3256ef6ff41d98472f7840ee5eda6791459f3
PrezesikSzef <127798900+PrezesikSzef@users.noreply.github.com>
2023-04-11 14:05:33 +0200

    Add files via upload

---
+++ sd.bat
@@ -0,0 +1,7 @@
+echo "# eloxdxd" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/PrezesikSzef/eloxdxd.git
+git push -u origin main
